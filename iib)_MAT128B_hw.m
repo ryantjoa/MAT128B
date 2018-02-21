c4ba18260e0f48c1833f8dc34f0284fe8a879ff4
Raymondcanchola <36659241+Raymondcanchola@users.noreply.github.com>
2018-02-20 19:05:22 -0800

    Update iib)_MAT128B_hw.m

--- iib)_MAT128B_hw.m
+++ iib)_MAT128B_hw.m
@@ -6,7 +6,8 @@
 %either phi(z) can stay bounded or increase towards indfinity.
 
 %%
-phi = @(z) z^2 -0.123 + 0.745i;
+c = -0.123 + 0.745i;
+phi = @(z) z^2 +c;
 fixpt1 = (1 + sqrt(6))/2;
 fixpt2 = (1 - sqrt(6))/2;
 colormap([1 0 0; 1 1 1]);
